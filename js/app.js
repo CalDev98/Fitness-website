aea80bdcbb55effac8e88a85721cf354d40dcf34
CalDev98 <64548197+CalDev98@users.noreply.github.com>
2020-11-03 15:21:11 +0100

    Add some more functionnalities

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Menu for mobile and tablets
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+	navLinks.classList.toggle("open");
+});
 
 /* Parallax effect */
 function parallax(element, distance, speed) {
@@ -8,3 +16,4 @@ function parallax(element, distance, speed) {
 window.addEventListener('scroll', function() {
     parallax(".hero-text", window.scrollY, 0.4);
 });
+
